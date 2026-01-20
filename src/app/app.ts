de43c8b3baa0686321e4c5f3ea9460fa0abbccac
Ton Nom <maive_@hotmail.com>
2026-01-19 22:44:07 -0500

    Hello World tuto terminé +doc lues

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,11 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
-  template: ` <h1>Default</h1> `,
+  template: ` <h1>Hello World</h1> `,
   styleUrls: ['./app.css'],
 })
 export class App {
-  title = 'default';
+  
 }
 
 // import { Component, signal } from '@angular/core';
