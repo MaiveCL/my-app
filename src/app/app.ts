9df4d6cb93b6fba8a300fa75dceeed519512a04c
Ton Nom <maive_@hotmail.com>
2026-01-19 22:02:32 -0500

    fichiers tutoriel ajouté, projet affichable sur internet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [],
+  template: ` <h1>Default</h1> `,
+  styleUrls: ['./app.css'],
 })
 export class App {
-  protected readonly title = signal('my-app');
+  title = 'default';
 }
+
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App {
+//   protected readonly title = signal('my-app');
+// }
