6cbcc3caa3ca61f6679d785fd024fcd583175b20
Ton Nom <maive_@hotmail.com>
2026-01-21 22:32:54 -0500

    Navigation / page de détails terminée

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housingService';
+import {HousingLocationInfo} from '../housinglocation';
 
 @Component({
   selector: 'app-details',
@@ -7,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './details.css',
 })
 export class Details {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocationId = -1;
+  housingLocation: HousingLocationInfo | undefined;
+  constructor() {
+    // POUR NE PAS AVOIR LE ID A -1 DAns l'affichage test
+    // this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
 }
