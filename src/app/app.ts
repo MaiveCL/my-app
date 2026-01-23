58727def593caab33988300bd970ee72a28370a4
Ton Nom <maive_@hotmail.com>
2026-01-23 18:32:46 -0500

    ménage css et améliorartion visuelles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,40 +1,43 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 //import {Home} from './home/home'; // demandé par le tuto, warning comme inutilisé dans la console
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
-  selector: 'app-root',
-  imports: [/*Home,*/ RouterOutlet, RouterLink, MatButtonModule], // demandé par le tuto, warning comme inutilisé dans la console
-  template:
-    `
-    <main>
-      <header class="brand-name">
-        <a [routerLink]="['/']">
-          <img class="brand-logo" src="logo.svg" alt="logo" aria-hidden="true" />
-        </a>
+    selector: 'app-root',
+    imports: [/*Home,*/ RouterOutlet, RouterLink, MatButtonModule], // demandé par le tuto, warning comme inutilisé dans la console
+    template:
+      `
+      <main>
+        <header class="brand-name">
+          <a [routerLink]="['/']">
+            <img class="brand-logo" src="logo.svg" alt="logo" aria-hidden="true" />
+          </a>
 
-        <button matButton="elevated" class="primary" type="button" (click)="goToLogin()">
-          Se&nbsp;connecter
-        </button>
-      </header>
+          <button matButton="elevated" class="primary" type="button" (click)="goToLogin()">
+            Se&nbsp;connecter
+          </button>
+        </header>
 
-      <section class="content">
-        <router-outlet />
-      </section>
-    </main>
-    `,
-  styleUrls: ['./app.css'],
-})
-export class App {
-  title = 'homes';
+        <section class="content">
+          <router-outlet />
+        </section>
+      </main>
+      `,
+    styleUrls: ['./app.css'],
+  })
+  export class App {
+    private readonly router = inject(Router);
+    title = 'homes';
 
-  goToLogin() { ///////////////////////////////////////////////////////////////////////////
-  window.location.href = '/login'; /// fait un refresh, c'est non, voir la mécanique du logo home
+    goToLogin() { 
+    this.router.navigate(['/login']);
+    //
+    // Ça c'est NON : fait un refresh, c'est non, voir la mécanique du logo home
+    // window.location.href = '/login'; 
+  }
 }
-}
-
 // import { Component, signal } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 
